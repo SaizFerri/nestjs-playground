1a64bdd26d92da97d7b32193442006681e21dd37
Unknown <adri_esp_11@hotmail.com>
2018-08-08 14:30:02 +0200

    feat: change port

--- src/main.hmr.ts
+++ src/main.hmr.ts
@@ -5,7 +5,8 @@ declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 
   if (module.hot) {
     module.hot.accept();
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
